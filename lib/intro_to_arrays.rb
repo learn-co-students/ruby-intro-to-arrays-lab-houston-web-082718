2cc3cf0880ec0bc990b5dcc1a9c9a07141ca0a6b
John Grubbs <johnmlowery@gmail.com>
2018-08-20 14:48:43 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,42 @@
+
+
+def instantiate_new_array
+  newArray = []
+end
+
+def array_with_two_elements 
+  newArray2 = ['the', 'one']
+end
+
+def first_element(array)
+  newArray2 = ["Welcome to New York", 'one']
+  return newArray2[0]
+end
+
+def third_element(array)
+newArray2 = ["Welcome to New York", 'one', "Style"]
+ return newArray2[2]
+ end
+  
+def last_element(array)
+  newArray2 = ["Welcome to New York", 'one', "Style", "Out of The Woods"]
+  return newArray2[3]
+end
+
+def first_element_with_array_methods(array)
+  newArray2 = ["Thailand", 'one', "Style", "Out of The Woods"]
+  return newArray2.shift
+end
+  
+def last_element_with_array_methods(array) 
+  newArray2 = ["Thailand", 'one', "Style", "Out of The Woods", "Myanmar"]
+  return newArray2.pop
+end
+
+def length_of_array(array)
+  newArray2 = ["Thailand", 'one', "Style", "Out of The Woods", "Myanmar", 1, 2 , 3]
+  return newArray2.length
+end
+  
+  
+  
